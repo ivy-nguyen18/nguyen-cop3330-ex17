725e2f36680ef7c42002bb2131a82371d59dd5d1
ivy-nguyen18 <nguyen.ivy1802@knights.ucf.edu>
2021-06-05 15:57:47 -0400

    Redid input statements

--- src/main/java/ex17/base/App.java
+++ src/main/java/ex17/base/App.java
@@ -12,21 +12,58 @@ public class App
     public static void main( String[] args )
     {
         Scanner input = new Scanner(System.in);
+        boolean flag = false;
+        double weight = 0;
+        int gender = 0;
+        double alcConsumed = 0;
+        double hours = 0;
 
-        System.out.print("Weight? ");
-        double weight = input.nextInt();
+        while(!flag){
+            System.out.print("Weight? ");
+            if(input.hasNextDouble()){
+                weight = input.nextDouble();
+                flag = true;
+            }
+            input.nextLine();
+        }
 
-        System.out.print("Gender? (1) for male, (2) for female ");
-        int gender = input.nextInt();
+        flag = false;
+        while(!flag){
+            System.out.print("Gender? (1) for male, (2) for female ");
+            if(input.hasNextInt()){
+                gender = input.nextInt();
+                if(gender == 1 || gender == 2){
+                    flag = true;
+                }
+            }
+            input.nextLine();
+        }
 
-        System.out.print("Total Alcohol Consumed (in ounces)? ");
-        double alcConsumed = input.nextDouble();
 
-        System.out.print("Hours since last drink? ");
-        double hours = input.nextDouble();
+        flag = false;
+        while(!flag){
+            System.out.print("Total Alcohol Consumed (in ounces)? ");
+            if(input.hasNextDouble()){
+                alcConsumed = input.nextDouble();
+                flag = true;
+            }
+            input.nextLine();
+        }
+
+
+        flag = false;
+        while(!flag){
+            System.out.print("Hours since last drink? ");
+            if(input.hasNextDouble()){
+                hours = input.nextDouble();
+                flag = true;
+            }
+            input.nextLine();
+        }
 
         BAC calculate = new BAC(weight, gender, alcConsumed, hours);
         double bac = calculate.calculateBAC();
+
 
         System.out.printf("Your BAC is %.2f", bac);
 
